1639f2d2232684df32dc6d74e1edd9de95dda246
mahikero <federicoparassarmiento@yahoo.com>
2016-11-28 08:26:19 -0500

    up

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -1,7 +1,23 @@
 function Application() {
     return (
-        <div className="application">
-        
+        <div className="scoreboard">
+            <div className="header">
+                <h1>Scoreboard</h1>
+            </div>
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">
+                        Rico
+                    </div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score"> 31 </div>
+                            <button className="counter-action increment"> + </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
